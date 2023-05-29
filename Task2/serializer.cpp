86d8daf894b51aa26392641c50cbcf14ad274b5f
Anton Ovcharenko <anton.ovcharenko@teamdev.com>
2023-05-29 19:14:00 +0300

    fix invalid json

--- Task2/serializer.cpp
+++ Task2/serializer.cpp
@@ -1,37 +1,37 @@
 #include "serializer.h"
 
 std::string ToXML(const std::string& tagName, const std::map<Key, std::string>& data, const std::string& indent) {
-	return indent + "<!-- XML representation of MAP -->\n";
+	return indent + "<!-- XML representation of MAP -->";
 }
 
 std::string ToXML(const std::string& tagName, const std::vector<std::string>& data, const std::string& indent) {
-	return indent + "<!-- XML representation of VECTOR -->\n";
+	return indent + "<!-- XML representation of VECTOR -->";
 }
 
 std::string ToXML(const std::string& tagName, const std::string& data, const std::string& indent) {
-	return indent + "<!-- XML representation of STRING -->\n";
+	return indent + "<!-- XML representation of STRING -->";
 }
 
 std::string ToJSON(const std::string& tagName, const std::map<Key, std::string>& data, const std::string& indent) {
-	return indent + "<!-- JSON representation of MAP -->\n";
+	return indent + "<!-- JSON representation of MAP -->";
 }
 
 std::string ToJSON(const std::string& tagName, const std::vector<std::string>& data, const std::string& indent) {
-	return indent + "<!-- JSON representation of VECTOR -->\n";
+	return indent + "<!-- JSON representation of VECTOR -->";
 }
 
 std::string ToJSON(const std::string& tagName, const std::string& data, const std::string& indent) {
-	return indent + "<!-- JSON representation of STRING -->\n";
+	return indent + "<!-- JSON representation of STRING -->";
 }
 
 std::string ToYAML(const std::string& tagName, const std::map<Key, std::string>& data, const std::string& indent) {
-	return indent + "<!-- YAML representation of MAP -->\n";
+	return indent + "<!-- YAML representation of MAP -->";
 }
 
 std::string ToYAML(const std::string& tagName, const std::vector<std::string>& data, const std::string& indent) {
-	return indent + "<!-- YAML representation of VECTOR -->\n";
+	return indent + "<!-- YAML representation of VECTOR -->";
 }
 
 std::string ToYAML(const std::string& tagName, const std::string& data, const std::string& indent) {
-	return indent + "<!-- YAML representation of STRING -->\n";
+	return indent + "<!-- YAML representation of STRING -->";
 }
--- Task2/serializer.h
+++ Task2/serializer.h
@@ -1,92 +1,120 @@
-#include <string>
-#include <vector>
 #include <map>
+#include <string>
 #include <variant>
+#include <vector>
 
 namespace {
-	typedef std::string Key;
-	const std::string INDENT_STEP = "  ";
-}
+    typedef std::string Key;
+    const std::string INDENT_STEP = "  ";
+} // namespace
 
-template<class...Ts>
+template <class... Ts> 
 struct Node;
 
-template<class...Ts>
+template <class... Ts>
 using NodeBase = std::variant<std::map<Key, Node<Ts...>>, Ts...>;
 
-template<class...Ts>
+template <class... Ts> 
 struct Node : NodeBase<Ts...> {
-	using NodeBase<Ts...>::NodeBase;
-	NodeBase<Ts...> const& base() const { return *this; }
-	NodeBase<Ts...>& base() { return *this; }
+    using NodeBase<Ts...>::NodeBase;
+    NodeBase<Ts...> const& base() const { return *this; }
+    NodeBase<Ts...>& base() { return *this; }
 };
 
-std::string ToXML(const std::string& tagName, const std::map<Key, std::string>& data, const std::string& indent = "");
+std::string ToXML(const std::string& tagName,
+                  const std::map<Key, std::string>& data,
+                  const std::string& indent = "");
 
-std::string ToXML(const std::string& tagName, const std::vector<std::string>& data, const std::string& indent = "");
+std::string ToXML(const std::string& tagName,
+                  const std::vector<std::string>& data,
+                  const std::string& indent = "");
 
-std::string ToXML(const std::string& tagName, const std::string& data, const std::string& indent = "");
+std::string ToXML(const std::string& tagName, const std::string& data,
+                  const std::string& indent = "");
 
-template<typename... Ts>
-std::string ToXML(const std::string& tagName, const std::map<Key, Node<Ts...>>& data, const std::string& indent = "") {
-	std::string result;
+template <typename... Ts>
+std::string ToXML(const std::string& tagName,
+                  const std::map<Key, Node<Ts...>>& data,
+                  const std::string& indent = "") {
+    std::string result;
 
-	result += "<" + tagName + ">\n";
+    result += "<" + tagName + ">";
 
-	for (const auto& pair : data) {
-		const auto& key = pair.first;
-		result += std::visit([key, &indent](const auto& data) { 
-			return ToXML(key, data, indent + INDENT_STEP); 
-			}, pair.second.base());
-	}
-	result += "</" + tagName + ">\n";
+    for (const auto& pair : data) {
+        result += "\n" + std::visit(
+            [&pair, &indent](const auto& data) {
+                return ToXML(pair.first, data, indent + INDENT_STEP);
+            }, pair.second.base());
+    }
 
-	return result;
+    result += "\n</" + tagName + ">\n";
+
+    return result;
 }
 
-std::string ToJSON(const std::string& tagName, const std::map<Key, std::string>& data, const std::string& indent = "");
+std::string ToJSON(const std::string& tagName,
+                   const std::map<Key, std::string>& data,
+                   const std::string& indent = "");
 
-std::string ToJSON(const std::string& tagName, const std::vector<std::string>& data, const std::string& indent = "");
+std::string ToJSON(const std::string& tagName,
+                   const std::vector<std::string>& data,
+                   const std::string& indent = "");
 
-std::string ToJSON(const std::string& tagName, const std::string& data, const std::string& indent = "");
+std::string ToJSON(const std::string& tagName,
+                   const std::string& data,
+                   const std::string& indent = "");
 
-template<typename... Ts>
-std::string ToJSON(const std::string& tagName, const std::map<Key, Node<Ts...>>& data, const std::string& indent = "") {
-	std::string result;
+template <typename... Ts>
+std::string ToJSON(const std::string& tagName,
+                   const std::map<Key, Node<Ts...>>& data,
+                   const std::string& indent = "") {
+    std::string result;
 
-	result += indent + tagName + ": {\n";
+    result += indent + '"' + tagName + '"' + ": {";
+    
+    for (auto it = data.cbegin(); it != data.cend(); ++it) {
+        result += "\n" + std::visit(
+            [&it, &indent](const auto &data) {
+                return ToJSON(it->first, data, indent + INDENT_STEP);
+            }, it->second.base());
 
-	for (const auto& pair : data) {
-		const auto& key = pair.first;
-		result += std::visit([key, &indent](const auto& data) { 
-			return ToJSON(key, data, indent + INDENT_STEP); 
-			}, pair.second.base());
-	}
-	result += indent + "}\n";
+        if (std::distance(it, data.cend()) != 1) {
+            result += ",";
+        }
+    }
 
-	return result;
+    result += "\n" + indent + "}\n";
+
+    return result;
 }
 
+std::string ToYAML(const std::string& tagName,
+                   const std::map<Key, std::string>& data,
+                   const std::string& indent = "");
 
-std::string ToYAML(const std::string& tagName, const std::map<Key, std::string>& data, const std::string& indent = "");
+std::string ToYAML(const std::string& tagName,
+                   const std::vector<std::string>& data,
+                   const std::string& indent = "");
 
-std::string ToYAML(const std::string& tagName, const std::vector<std::string>& data, const std::string& indent = "");
+std::string ToYAML(const std::string& tagName, const std::string& data,
+                   const std::string& indent = "");
 
-std::string ToYAML(const std::string& tagName, const std::string& data, const std::string& indent = "");
+template <typename... Ts>
+std::string ToYAML(const std::string& tagName,
+                   const std::map<Key, Node<Ts...>>& data,
+                   const std::string& indent = "") {
+    std::string result;
 
-template<typename... Ts>
-std::string ToYAML(const std::string& tagName, const std::map<Key, Node<Ts...>>& data, const std::string& indent = "") {
-	std::string result;
+    result += indent + tagName + ":";
 
-	result += indent + tagName + ":\n";
+    for (const auto& pair: data) {
+        result += "\n" + std::visit(
+            [&pair, &indent](const auto& data) {
+                return ToYAML(pair.first, data, indent + INDENT_STEP);
+            }, pair.second.base());
+    }
 
-	for (const auto& pair : data) {
-		const auto& key = pair.first;
-		result += std::visit([key, &indent](const auto& data) { 
-			return ToYAML(key, data, indent + INDENT_STEP); 
-			}, 
-			pair.second.base());
-	}
+    result += "\n";
 
-	return result;
+    return result;
 }
